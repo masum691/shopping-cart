ed811987bcd43341b817b1c96211df073cf84e70
masum691 <masumhu691@gmail.com>
2021-08-19 22:06:10 +0600

    added subtotal, tax and total

--- app.js
+++ app.js
@@ -17,7 +17,25 @@ function updateProductNumber(product, price, isIncreasing){
     // update total
     const productTotal = document.getElementById(product+ '-total');
     productTotal.innerText = productNumber * price;
-
+    // calculate total 
+    calculateTotal();
+}
+function getInputValue(product){
+    const productNumber = document.getElementById('phone-number');
+    const phoneNumber = parseInt(productNumber.value);
+    return phoneNumber;
+}
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+    
 }
 // phone increase and decrease events
 document.getElementById('phone-plus').addEventListener('click', function(){
@@ -34,14 +52,4 @@ document.getElementById('case-plus').addEventListener('click', function(){
 document.getElementById('case-minus').addEventListener('click', function(){
     updateProductNumber('case', 59, false);
 })
-
-
-
-
-
-
-
-
-
-
 
